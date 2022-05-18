fc54c0521b82a0e6ee282f93350861d569dc0851
linfeng@crypto.com <linfeng@crypto.com>
2022-05-18 12:25:40 +0800

    fix mempool

--- integration_tests/test_mempool.py
+++ integration_tests/test_mempool.py
@@ -33,7 +33,6 @@ def test_mempool(cronos):
     receipt = w3.eth.wait_for_transaction_receipt(txhash)
     assert receipt.status == 1
     assert filter.get_new_entries() == [txhash]
-    assert 1 == 0
 
     # send many txs to mempool
     # for i in range(0, 100):
